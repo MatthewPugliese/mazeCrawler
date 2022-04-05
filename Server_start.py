9c91973b6218a70c01b71e1f1c14ab2f5b9413e0
atgreene <60367393+atgreene@users.noreply.github.com>
2022-04-05 15:17:14 -0400

    Update Server_start.py

--- Server_start.py
+++ Server_start.py
@@ -10,28 +10,33 @@ def server_program():
     maze = labyrinth.getMaze()
     goal = labyrinth.getGoal()
     maze_walls = labyrinth.getWalls()
-
+    print("1")
     screen = pygame.display.set_mode((1395, 1100))
     screen.fill((0, 0, 0))
     color = (0, 128, 255)
     pygame.draw.rect(screen, (0, 255, 0), goal) # finish
     maze.draw(goal)
     pygame.draw.rect(screen, (138,43,226), pygame.Rect(16,16,10,10))
-
+    print("2")
     message = [maze,goal]
     message = pickle.dumps(message)
     # get the hostname
     host = socket.gethostname()
     port = 2000  # initiate port no above 1024
-
+    print("3")
     server_socket = socket.socket()  # get instance
     server_socket.bind((host, port))  # bind host address and port together
     server_socket.listen(1)
-    #while True:
-        #conn, address = server_socket.accept()  # accept new connection
-        #conn.send(message)
-        
+    #conn, address = server_socket.accept()  # accept new connection
+    #conn.send(message)
+    print("4")
     #conn.close()
+
+    while True:
+        conn, address = server_socket.accept()  # accept new connection
+        conn.send(message)
+        
+    conn.close()
 
 if __name__ == '__main__':
     server_program()
