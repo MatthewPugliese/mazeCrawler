728f0f6afb6ae2d260a50e4c531356f8af81bf4d
shaneknopp <sknopp@colgate.edu>
2022-05-03 18:09:59 -0400

    partially commented and organized

--- Server_start.py
+++ Server_start.py
@@ -7,8 +7,46 @@ import concurrent.futures
 import time
 import random
 
+def server_program():
+    """
+    Principal function that initiates the server and accepts client connections
+    """  
+    maze = labyrinth.getMaze()
+    goal = labyrinth.getGoal()
+    player_colors = [[255,188,66],[216,17,89],[226,160,255],[183,255,216],[255,220,204],[251,99,118],[100,245,141],[255,202,58],[138,201,38],[247,99,0],[237,37,78],[3,252,86],[233,223,0]]
+    player_coords = {}
+
+    difficulty = False
+    message = [maze,goal,difficulty]
+    message = pickle.dumps(message)
+    message += b"746869736973746865656e64"
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    host = s.getsockname()[0]
+    print("Host IP is:", host)
+    
+    port = 2001  # initiate port above 1024
+    server_socket = socket.socket()  # get instance
+    server_socket.bind((host, port))  # bind host address and port together
+    server_socket.listen(5)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        while True:
+            client_sock, client_addr = server_socket.accept()
+            print("Client Connected")
+            executor.submit(handle_client, client_sock, message, player_coords)
+            executor.submit(data_receiver, client_sock, player_coords, client_addr, player_colors)
 
 def data_receiver(client_socket, dict, client_addr, player_colors):
+    """
+    Threaded function to handle client coordinate updates
+
+    :param client_socket: the socket a particular client is using
+    :param dict: dictionary containing the current coordinates for every client (key client_addr, value array of coordinates)
+    :param client_addr: the IP address of a connection
+    :param player_colors: array containing unique colors in RGB format
+    """    
     player_color = random.choice(player_colors)
     player_colors.remove(player_color)
     while(True):
@@ -27,51 +65,20 @@ def data_receiver(client_socket, dict, client_addr, player_colors):
             cord_array.append(player_color)
             dict[client_addr] = cord_array
 
-
-def server_program():
-    maze = labyrinth.getMaze()
-    goal = labyrinth.getGoal()
-    player_colors = [[255,188,66],[216,17,89],[226,160,255],[183,255,216],[255,220,204],[251,99,118],[100,245,141],[255,202,58],[138,201,38],[247,99,0],[237,37,78],[3,252,86],[233,223,0]]
-    player_Chords = {}
-
-
-    difficulty = False
-    message = [maze,goal,difficulty]
-    message = pickle.dumps(message)
-    message += b"746869736973746865656e64"
-    #host = "149.43.198.248"
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    host = s.getsockname()[0]
-    print("Host IP is:", host)
-    
-
-    port = 2001  # initiate port no above 1024
-    server_socket = socket.socket()  # get instance
-    server_socket.bind((host, port))  # bind host address and port together
-    server_socket.listen(5)
-
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        while True:
-            client_sock, client_addr = server_socket.accept()
-            print("Client Connected")
-            executor.submit(handle_client, client_sock, message, player_Chords)
-            executor.submit(data_receiver, client_sock, player_Chords, client_addr, player_colors)
-    
-
 def handle_client(sock, message, dict):
+    """
+    Threaded function to initialize and update clients 
+
+    :param sock: the socket a particular client is using
+    :param message: the maze and goal as a pickled object
+    :param dict: the dictionary containing all player coordinates
+    """  
     sock.send(message) #the maze and goal are sent to the client
     while True:
-            draw_players(dict, sock)
-            time.sleep(.03)
-        
-
-def draw_players(dict, sock):
-    cord_msg = pickle.dumps(dict)
-    cord_msg += b"746869736973746865656e64"
-    sock.send(cord_msg) 
+        cord_msg = pickle.dumps(dict)
+        cord_msg += b"746869736973746865656e64"
+        sock.send(cord_msg) 
+        time.sleep(.03)
 
 if __name__ == '__main__':
     server_program()
